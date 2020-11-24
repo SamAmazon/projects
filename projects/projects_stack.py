512643801e4a2c6ed98cdaee11c04f4c435f85b4
Shankar <sam.amazon501@gmail.com>
2020-11-23 21:25:12 -0700

    Inital Commit

--- projects/projects_stack.py
+++ projects/projects_stack.py
@@ -1,4 +1,7 @@
-from aws_cdk import core
+from aws_cdk import (
+    core,
+    aws_s3 as _s3
+)
 
 
 class ProjectsStack(core.Stack):
@@ -7,3 +10,10 @@ class ProjectsStack(core.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        _s3.Bucket(
+            self,
+            "myBucketId",
+            bucket_name="com.samraja.projectsbucket",
+            versioned=True,
+            encryption=_s3.BucketEncryption.KMS_MANAGED
+        )
